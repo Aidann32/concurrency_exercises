aabfba30353b8658cfd06f5b4458438d60c9c422
aliai <aliaidan02@gmail.com>
2024-12-25 22:42:17 +0500

    Matrix struct final

--- exercises/matrix/matrix.go
+++ exercises/matrix/matrix.go
@@ -1,17 +1,38 @@
 package matrix
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"sync"
+)
 
-func checkMultiplicability(a [][]int, b [][]int) (bool, error) {
-	bRows := len(b)
-	aCols := len(a[0])
-	bCols := len(b[0])
-	for _, arr := range a {
+type matrixIndex struct {
+	I int
+	J int
+}
+
+type Multiplication struct {
+	A            [][]int
+	B            [][]int
+	P            int
+	resultMatrix [][]int
+	resultAccess [][]bool
+
+	wg    *sync.WaitGroup
+	mutex *sync.RWMutex
+	jobs  chan matrixIndex
+}
+
+func (m *Multiplication) checkMultiplicability() (bool, error) {
+	bRows := len(m.B)
+	aCols := len(m.A[0])
+	bCols := len(m.B[0])
+	for _, arr := range m.A {
 		if len(arr) != aCols {
 			return false, errors.New("a is not matrix")
 		}
 	}
-	for _, arr := range b {
+	for _, arr := range m.B {
 		if len(arr) != bCols {
 			return false, errors.New("b is not matrix")
 		}
@@ -22,16 +43,70 @@ func checkMultiplicability(a [][]int, b [][]int) (bool, error) {
 	return true, nil
 }
 
-func calculateResultElement(a [][]int, b [][]int, resRowIndex, resColIndex int) int {
+func (m *Multiplication) calculateResultElement(resRowIndex, resColIndex int) int {
 	result := 0
-	for i := 0; i < len(a[0]); i++ {
-		result += a[resRowIndex][i] * b[i][resColIndex]
+	for i := 0; i < len(m.A[0]); i++ {
+		result += m.A[resRowIndex][i] * m.B[i][resColIndex]
+		fmt.Printf("Calculating A[%d][%d](%d) and B[%d][%d](%d) result is %d\n",
+			resRowIndex, i, m.A[resRowIndex][i], i, resColIndex, m.B[i][resColIndex], result)
 	}
 	return result
 }
 
-func multiplyMatrices(a [][]int, b [][]int, p int) ([][]int, error) {
-	isMultiplicable, err := checkMultiplicability(a, b)
+func (m *Multiplication) getAccessMatrixElement(i, j int) bool {
+	defer m.mutex.RUnlock()
+	m.mutex.RLock()
+	return m.resultAccess[i][j]
+}
+
+func (m *Multiplication) setAccessMatrixElement(i, j int, value bool) {
+	defer m.mutex.Unlock()
+	m.mutex.Lock()
+	m.resultAccess[i][j] = value
+}
+
+func (m *Multiplication) worker() {
+	for j := range m.jobs {
+		defer m.wg.Done()
+		indexes := j
+		if !m.getAccessMatrixElement(indexes.I, indexes.J) {
+			m.setAccessMatrixElement(indexes.I, indexes.J, true)
+			result := m.calculateResultElement(indexes.I, indexes.J)
+			m.resultMatrix[indexes.I][indexes.J] = result
+		}
+	}
+}
+
+func (m *Multiplication) multiplyMatrices() {
+
+	// Worker pool of p goroutines which check accessMatrix and calculate c[i][j] if accessMatrix[i][j] is false
+	// true means that element c[i][j] is calculated or calculating
+
+	m.wg.Add(len(m.resultMatrix) * len(m.resultMatrix[0]))
+
+	for i := 1; i <= m.P; i++ {
+		go m.worker()
+	}
+
+	for i := 0; i < len(m.resultMatrix); i++ {
+		for j := 0; j < len(m.resultMatrix[0]); j++ {
+			m.jobs <- matrixIndex{I: i, J: j}
+		}
+	}
+	close(m.jobs)
+	m.wg.Wait()
+}
+
+func NewMultiplication(a, b [][]int, p int) *Multiplication {
+	return &Multiplication{
+		A: a,
+		B: b,
+		P: p,
+	}
+}
+
+func (m *Multiplication) Run() ([][]int, error) {
+	isMultiplicable, err := m.checkMultiplicability()
 	if !isMultiplicable {
 		return nil, errors.New("matrices are not multiplicable")
 	}
@@ -39,24 +114,18 @@ func multiplyMatrices(a [][]int, b [][]int, p int) ([][]int, error) {
 		return nil, err
 	}
 
-	cRows, cCols := len(a), len(b[0])
-	resultAccess := make([][]bool, cRows)
-	result := make([][]int, cRows)
-	for i := range resultAccess {
-		resultAccess[i] = make([]bool, cCols)
-		result[i] = make([]int, 0)
+	cRows, cCols := len(m.A), len(m.B[0])
+	m.resultAccess = make([][]bool, cRows)
+	m.resultMatrix = make([][]int, cRows)
+	for i := range m.resultAccess {
+		m.resultAccess[i] = make([]bool, cCols)
+		m.resultMatrix[i] = make([]int, cCols)
 	}
 
-	// Worker pool of p goroutines which check accessMatrix and calculate c[i][j] if accessMatrix[i][j] is false
-	// true means that element c[i][j] is calculated or calculating
+	m.jobs = make(chan matrixIndex, cRows*cCols)
+	m.wg = &sync.WaitGroup{}
+	m.mutex = &sync.RWMutex{}
 
-	return result, nil
-}
-
-func Run(goroutineNumber int, a [][]int, b [][]int) ([][]int, error) {
-	result, err := multiplyMatrices(a, b, goroutineNumber)
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
+	m.multiplyMatrices()
+	return m.resultMatrix, nil
 }
--- main.go
+++ main.go
@@ -1,9 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Aidann32/concurrency_exercises/exercises/files"
+	"github.com/Aidann32/concurrency_exercises/exercises/matrix"
 )
 
 func main() {
 	files.Run(10)
+
+	matrixA := [][]int{
+		{1, 2},
+		{3, 4},
+	}
+	matrixB := [][]int{
+		{5, 6},
+		{7, 8},
+	}
+	multiplication := matrix.NewMultiplication(matrixA, matrixB, 10)
+	fmt.Println(multiplication.Run())
+
+	matrixA = [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+	}
+	matrixB = [][]int{
+		{7, 8},
+		{9, 10},
+		{11, 12},
+	}
+	multiplication = matrix.NewMultiplication(matrixA, matrixB, 10)
+	fmt.Println(multiplication.Run())
+
+	matrixA = [][]int{
+		{2, 0, -1},
+		{3, 5, 2},
+		{1, 4, 3},
+	}
+	matrixB = [][]int{
+		{1, 2, 3},
+		{4, 0, 6},
+		{7, 8, 9},
+	}
+	multiplication = matrix.NewMultiplication(matrixA, matrixB, 10)
+	fmt.Println(multiplication.Run())
 }
